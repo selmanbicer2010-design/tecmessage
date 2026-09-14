f642495b54bb5f75766e6329cdea506d662c863b
selmanbicer2010-design <selmanbicer2010@gmail.com>
2026-09-14 02:49:01 -0400

    better run.bat

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 run .\run.bat on windows to run
+.\server\build.bat to build server individually
+.\client\build.bat to build client individually
+ts frontend and cpp backend
 linux support later next decade
 
 dependencies: boost.asio and boost.beast
--- run.bat
+++ run.bat
@@ -1,7 +1,30 @@
 cls
-cmake --build .\server\build
+pushd client
+call npm.cmd exec tsc
+if %errorlevel% neq 0 (
+    echo TypeScript build failed
+    popd
+    pause
+    cls
+    exit /b 1
+)
+popd
 pause
+
 cls
+cmake --build .\server\build
+if %errorlevel% neq 0 (
+    echo CMake build failed
+    pause
+    cls
+    exit /b 1
+)
+
+cls
+echo Opening in browser...
+start http://localhost:8080
+timeout /t 1 /nobreak >nul
+echo Starting server...
 .\server\build\tecmessage.exe
 pause
 cls
